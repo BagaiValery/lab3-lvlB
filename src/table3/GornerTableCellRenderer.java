c47053b3b8c4202e381cfd611ae13759d6a3dfe9
ValeryBagai <lerika1307@mail.ru>
2020-11-23 11:51:46 +0300

    lab3

---
+++ src/table3/GornerTableCellRenderer.java
@@ -0,0 +1,72 @@
+package table3;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class GornerTableCellRenderer implements TableCellRenderer {
+	
+	// Ищем ячейки, строковое представление которых равно needle (иголке)
+	// Применяется аналогия поиска иголки в сене, в роли сена - таблица
+	private String needle = null;
+	public void setNeedle(String needle) {
+	this.needle = needle;
+	}
+	
+	private DecimalFormat formatter = (DecimalFormat)NumberFormat.getInstance();
+	private JPanel panel = new JPanel();
+	private JLabel label = new JLabel();
+	
+	public GornerTableCellRenderer() {
+	// Разместить надпись внутри панели
+	panel.add(label);
+	// Установить выравнивание надписи по левому краю панели
+	panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+	// Показывать только 5 знаков после запятой
+	formatter.setMaximumFractionDigits(5);
+	// Не использовать группировку (не отделять тысячи)
+	// Т.е. показывать число как "1000", а не "1 000" или "1,000"
+	formatter.setGroupingUsed(false);
+	// Установить в качестве разделителя дробной части точку, а не запятую
+	// По умолчанию дробная часть
+	// отделяется запятой
+	DecimalFormatSymbols dottedDouble = formatter.getDecimalFormatSymbols();
+	dottedDouble.setDecimalSeparator('.');
+	formatter.setDecimalFormatSymbols(dottedDouble);
+	}
+	
+	public Component getTableCellRendererComponent(JTable table, Object
+			value, boolean isSelected, boolean hasFocus, int row, int col) {
+			// Преобразовать число в строку с помощью форматировщика
+			String formattedDouble = formatter.format(value);
+			String razdel = "\\.";
+			String[] str;
+			String copy = formattedDouble + ".0";
+			str = copy.split(razdel);
+			int _int=Integer.parseInt(str[0]);
+			
+			if (col==1 && _int%2==0)panel.setBackground(Color.ORANGE);
+			else panel.setBackground(Color.WHITE);
+			
+			if (col==1 && needle!=null && needle.equals(formattedDouble)) {
+			// Номер столбца = 1 (т.е. второй столбец)
+			// + иголка не null (т.е. мы что-то ищем)
+			// + значение иголки совпадает со значением ячейки таблицы -
+			// окрасить задний фон панели в красный цвет
+			panel.setBackground(Color.PINK);
+			}
+			// Установить текст надписи равным строковому представлению числа
+			label.setText(formattedDouble);
+			
+			return panel;
+			}
+
+}
---
+++ src/table3/GornerTableModel.java
@@ -0,0 +1,108 @@
+package table3;
+
+import javax.swing.table.AbstractTableModel;
+
+public class GornerTableModel extends AbstractTableModel {
+	
+	private Double[] coefficients;
+	private Double from;
+	private Double to;
+	private Double step;
+	private Boolean fl;
+	private Double result;
+	
+	public GornerTableModel(Double from, Double to, Double step, Double[]
+			coefficients) {
+			this.from = from;
+			this.to = to;
+			this.step = step;
+			this.coefficients = coefficients;
+			}
+	
+	public Double getFrom() {
+		return from;
+		}
+		public Double getTo() {
+		return to;
+		}
+		public Double getStep() {
+		return step;
+		}
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex) {
+		 if (columnIndex == 2) return Boolean.class;
+		  else return Double.class; 
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		case 0:
+		return "Значение X";
+		case 1:
+		return "Значение многочлена";
+		default:  
+		return "Две пары";
+		}
+	}
+
+	@Override
+	public int getColumnCount() {
+		return 3;
+	}
+
+	@Override
+	public int getRowCount() {
+		return new Double(Math.ceil((to-from)/step)).intValue()+1;
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		// Вычислить значение X как НАЧАЛО_ОТРЕЗКА + ШАГ*НОМЕР_СТРОКИ
+		double x = from + step*row;
+		switch (col) {
+		case 0:
+		return x;
+		case 1:
+		 {
+	        result=0.0;
+
+	        for(int i=0;i<coefficients.length;i++)
+	        {
+	            result+=Math.pow(x, coefficients.length-1-i)*coefficients[i];
+	        }
+	        return result;
+		}
+		default:
+		{	
+			fl = false;
+			int schet=0;
+			int cheslo2, cheslodrob2;
+			double copy2=result;
+			double copy = result;
+			do
+			{
+				int cheslo=(int)(copy%10);
+				
+				copy=copy/10; 
+				cheslo2=(int)(copy%10);
+				if (cheslo==cheslo2)schet++;
+				
+				if(result*10!=0 && ((copy2*10)%10)!=0)
+				{	int cheslodrob = (int)(result*10)%10;
+				copy2=copy2*10;
+				cheslodrob2=(int)(copy2%10);
+				if (cheslodrob==cheslodrob2)schet++;
+				}
+				
+				if(schet>=2) {fl=true; schet=0;}
+			}while(cheslo2!=0);
+			return fl;
+		}
+		}
+		 
+
+	}
+
+}
